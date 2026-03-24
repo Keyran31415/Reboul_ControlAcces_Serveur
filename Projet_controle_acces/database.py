fc3b10ab33b500684e4e40dabd749ba5bb27029b
Keyran31415 <keyran.re@lycee-de-beauvoir.education>
2026-03-24 13:29:05 +0100

    Update database.py

--- Projet_controle_acces/database.py
+++ Projet_controle_acces/database.py
@@ -4,9 +4,9 @@ def get_connection():
     try:
         connection = pymysql.connect(
             host="mysql-key314.alwaysdata.net",
-            user="key314_controle_acces",
+            user="key314_securite_nn",
             password="Key31415",
-            database="key314_controle_acces",
+            database="key314_securite_nn",
             charset="utf8",
             cursorclass=pymysql.cursors.DictCursor
         )
